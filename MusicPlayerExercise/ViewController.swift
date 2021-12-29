60b51fc5060acb48e587ca3fffeb1ece90046307
Wen Lo <luowen0616@gmail.com>
2021-12-29 10:12:12 +0800

    Initial Commit

---
+++ MusicPlayerExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayerExercise
+//
+//  Created by Wen Luo on 2021/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
